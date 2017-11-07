f9b9494fe4600b1951570f5698196f0f659d7cfd
JMB2K <jonathanmb2000@outlook.com>
2017-11-07 13:02:10 -0600

    Adding the file to use if using ARU logs

--- report_adj.py
+++ report_adj.py
@@ -10,60 +10,68 @@ labels = reports["Label"]
 working_dict = OD()
 
 def get_job_num(file):
-	"""
-	This is splitting the data
-	to get the plp job number
-	"""
-	x = file.split('\\')
-	return x[3]
+    """
+    This is splitting the data
+    to get the plp job number
+    """
+    x = file.split('\\')
+    return x[3]
 
 
 def main():
-	"""
-	So much looping!  But this is
-	sorting through all files and
-	organizing by PLP job number,
-	then adding all files printed
-	in that job to a list and counting
-	how many times the first file name
-	appears in that list to show how
-	many sets were printed, and returning
-	that info in a dict.
-	"""
-	temp_dict = OD()
-	for jobs in labels:  # Going through labels and pulling the PLP job number, then adding to dict
-		job_num=get_job_num(jobs)
-		if not job_num in temp_dict:
-			temp_dict[job_num] = []
+    """
+    So much looping!  But this is
+    sorting through all files and
+    organizing by PLP job number,
+    then adding all files printed
+    in that job to a list and counting
+    how many times the first file name
+    appears in that list to show how
+    many sets were printed, and returning
+    that info in a dict.
+    """
+    temp_dict = OD()
+    for jobs in labels:  # Going through labels and pulling the PLP job number, then adding to dict
+        job_num=get_job_num(jobs)
+        if not job_num in temp_dict:
+            temp_dict[job_num] = []
 
-	for job_nums in temp_dict:  # Going through the temp dict getting the PLP job numbers
-		temp_list = []
-		for x in labels:  # Going through labels and putting all files from PLP job number into a list
-			if get_job_num(x) == job_nums:
-				temp_list.append(x)
-		temp_dict[job_nums] = temp_list  # The PLP job number is the key and the list of files printed is the value
-	for job_nums in temp_dict:
-		count = 0
-		checker = temp_dict[job_nums][0]  # The first file in the list is set as a check point
-		for last_one in temp_dict[job_nums]:  # Itterating the list value and checking how many times the first file shows up
-			if last_one == checker:
-				count += 1
-		working_dict[job_nums] = count  # The PLP job number is the key and the number of sets printed is the value
-	final_dict = OD()
-	for key, value in working_dict.items():
-		if value > 1:
-			final_dict[key] = value
-	return final_dict
+    for job_nums in temp_dict:  # Going through the temp dict getting the PLP job numbers
+        temp_list = []
+        for x in labels:  # Going through labels and putting all files from PLP job number into a list
+            if get_job_num(x) == job_nums:
+                temp_list.append(x)
+        temp_dict[job_nums] = temp_list  # The PLP job number is the key and the list of files printed is the value
+    for job_nums in temp_dict:
+        count = 0
+        checker = temp_dict[job_nums][0]  # The first file in the list is set as a check point
+        for last_one in temp_dict[job_nums]:  # Itterating the list value and checking how many times the first file shows up
+            if last_one == checker:
+                count += 1
+        working_dict[job_nums] = count  # The PLP job number is the key and the number of sets printed is the value
+    final_dict = OD()
+    for key, value in working_dict.items():
+        if value > 1:
+            final_dict[key] = value
+    return final_dict
 
 
 if __name__ == "__main__":
-	start = time.time()
-	final_dict = main()
-	new_line = 0
-	for key, value in sorted(final_dict.items()):
-		print("{} - {} sets".format(key, value), end='\t')
-		new_line += 1
-		if new_line % 3 == 0:
-			print('\n')
-	elapsed = time.time() - start
-	print("\nThere are {} jobs, sorted in {:.2f} seconds".format(len(final_dict), elapsed))
+    start = time.time()
+    final_dict = main()
+    new_line = 0
+    to_print = ''
+    for key, value in sorted(final_dict.items()):
+        print("{} - {} sets".format(key, value), end='\t')
+        to_print = to_print + "{} - {} sets".format(key, value) + '\t\t\t'
+        new_line += 1
+        if new_line % 3 == 0:
+            print('\n')
+            to_print = to_print + '\n'
+    elapsed = time.time() - start
+    print("\nThere are {} jobs, sorted in {:.2f} seconds".format(len(final_dict), elapsed))
+    to_print = to_print + "\nThere are {} jobs, sorted in {:.2f} seconds".format(len(final_dict), elapsed)
+    with open('C:\\Users\\00015\\Desktop\\MultiSets.txt', 'w') as f:
+        f.write(to_print)
+
+
---
+++ reports_adj_ARU.py
@@ -0,0 +1,75 @@
+
+import pandas as pd
+import time
+
+
+reader = pd.read_csv('C:\\Users\\00015\\Desktop\\ARU.log', low_memory=False)
+data = reader[["JOB_FILENAME", "IMAGE_FILENAME"]]  # Get the only data we need
+data_dict=dict()
+
+def organize(data):
+    """
+    This is putting everything into a dict
+    with the job number as the key and
+    every file printed as a list for the
+    value
+    """
+    temp=[]
+    for job, file in data.iterrows():
+        jobname=file["JOB_FILENAME"][:-4]
+        temp.append([jobname, file['IMAGE_FILENAME']])
+        if not jobname in data_dict:
+            data_dict[jobname] = []  # creating keys in dict
+    for plp in data_dict:
+        temp2=[]  # Making a list of all files printed in job
+        for jobs in temp:
+            if jobs[0] == plp:
+                temp2.append(jobs[1])
+        data_dict[plp]=temp2  # Job number = list of files printed
+    return data_dict
+
+
+def count_sets(data_dict):
+    """
+    Here we just iterate through the
+    job numbers, then lists of files.
+    Everytime the first file is repeated
+    we up the set count - then throw the
+    job number and set count into another
+    dict
+    """
+    final_dict=dict()
+    for job in data_dict:  #  Iterate job numbers
+        count = 0
+        checkfile = data_dict[job][0]  # Anytime this shows up, that's one set printed
+        for file in data_dict[job]:  # Iterate files for above job number
+            if file == checkfile:
+                count+=1
+        if count > 1:
+            final_dict[job]=count
+    return final_dict
+
+
+if __name__ == '__main__':
+    """
+    Just a bunch of bs to get it to print
+    and write to a file so I can easily
+    see it to make my changes in Argos
+    """
+    start=time.time()
+    first=organize(data)
+    result=count_sets(first)
+    new_line = 0
+    to_print = ''
+    for key, value in sorted(result.items()):
+        print(key, '-', value, 'sets', end='\t')
+        to_print = to_print + "{} - {} sets".format(key, value) + '\t\t\t'
+        new_line += 1
+        if new_line % 3 == 0:
+            print('\n')
+            to_print = to_print + '\n'
+    elapsed = time.time() - start
+    print("\nThere are {} jobs, sorted in {:.2f} seconds".format(len(result), elapsed))
+    to_print = to_print + "\nThere are {} jobs, sorted in {:.2f} seconds".format(len(result), elapsed)
+    with open('C:\\Users\\00015\\Desktop\\MultiSets.txt', 'w') as f:
+        f.write(to_print)
